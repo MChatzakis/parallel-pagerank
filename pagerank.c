8412a95591758098afaa3bc75f2cd00c47c1d6f7
chatzakis <chatzakis@ics.forth.gr>
2021-03-14 18:44:04 +0200

    list

--- pagerank.c
+++ pagerank.c
@@ -3,98 +3,70 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#include "graph/graph.h"
+#include "graph.h"
 
 #define LINE_SIZE 100
-#define THREADS_NUM 4
+#define THREAD_NUM 10
 #define D_FACTOR 0.85
-#define ITERATIONS 50
-#define DEBUG 0
 
 typedef struct thread_data
 {
-    long from;
-    long to;
+    node_t *from;
+    node_t *to;
 } thread_data;
 
-pthread_mutex_t mutex;
-pthread_barrier_t bar;
-pthread_attr_t attr;
+pthread_t threads[THREAD_NUM];
+thread_data data[THREAD_NUM];
 
-graph_t *g;
-
-pthread_t threads[THREADS_NUM];
-thread_data data[THREADS_NUM];
-
-graph_t *read_file(char *filename);
-void *pagerank_calculate(void *arg);
+void read_csv(char *filename, graph_t *g);
 void write_csv(char *filename, graph_t *g);
 
-int main()
+void *calculate_pagerank(void *arg);
+
+int main(int argc, char **argv)
 {
-    long i, sets = 0, from = 0, to = 0;
+    graph_t *g;
+    node_t *from, *to, *iter;
+    char *filename = "datasets/enron.txt";
+    double d_factor = 0.85, init_pr = 1.0;
+    int border = 0, i, counter = 0;
 
-    g = read_file("datasets/enron.txt");
-    sets = g->size / THREADS_NUM;
+    g = graph_initialize(init_pr);
 
-    pthread_mutex_init(&mutex, NULL);
-
-    pthread_barrier_init(&bar, NULL, THREADS_NUM);
-
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-    for (i = 0; i < THREADS_NUM; i++)
+    /*iter = g->head;
+    for (i = 0; i < THREAD_NUM; i++)
     {
-        from = i * sets;
-        to = (i + 1) * sets;
+        border = (g->size) / THREAD_NUM;
+        from = iter;
+        while (iter != NULL && counter < border)
+        {
+            iter = iter->next;
+        }
+        to = iter;
+        //find borders ...
         data[i].from = from;
         data[i].to = to;
+        pthread_create(&threads[i], NULL, calculate_pagerank, (void *)data[i]);
     }
 
-    if ((sets = g->size % THREADS_NUM) > 0)
-    {
-        data[THREADS_NUM - 1].to = g->size;
-    }
-
-    for (i = 0; i < THREADS_NUM; i++)
-    {
-        //printf("Thread %ld takes %ld to %ld.\n", i, data[i].from, data[i].to);
-        pthread_create(&threads[i], &attr, pagerank_calculate, &data[i]);
-    }
-
-    for (i = 0; i < THREADS_NUM; i++)
+    for (i = 0; i < THREAD_NUM; i++)
     {
         pthread_join(threads[i], NULL);
     }
+    */
+    read_csv(filename, g);
 
-    //graph_print(g, stdout);
-    write_csv("output/pagerank.csv", g);
+    //graph_print_nodes(g);
+    //write_csv("pagerank.csv", g);
+
     return 0;
 }
 
-void write_csv(char *filename, graph_t *g)
+void read_csv(char *filename, graph_t *g)
 {
     FILE *stream;
-    if (!(stream = fopen(filename, "w+")))
-    {
-        perror("Could not open the file");
-        exit(EXIT_FAILURE);
-    }
-
-    graph_csv(g, stream);
-
-    fclose(stream);
-
-    printf("File saved: \"%s\"\n", filename);
-}
-
-graph_t *read_file(char *filename)
-{
-    FILE *stream;
-    graph_t *g;
     char line[LINE_SIZE], *tok;
-    long from, to, size = 0;
+    long from, to;
     int counter = 0;
 
     if (!(stream = fopen(filename, "r+")))
@@ -103,7 +75,7 @@ graph_t *read_file(char *filename)
         exit(EXIT_FAILURE);
     }
 
-    while ((fgets(line, LINE_SIZE, stream)) != NULL)
+    while ((fgets(line, LINE_SIZE, stream)) != NULL) // && counter < 20)
     {
         if (line[0] == '#')
         {
@@ -113,99 +85,33 @@ graph_t *read_file(char *filename)
         tok = strtok(line, "\r \t\v");
         from = atol(tok);
 
-        if (size < from)
-        {
-            size = from;
-        }
+        //printf("From %ld", from);
+        //printf("From %s ", tok);
 
         tok = strtok(NULL, "\r \t\v");
         to = atol(tok);
-        if (size < to)
-        {
-            size = to;
-        }
-    }
-    size++;
-    printf("Nodes: %ld\n", size);
 
-    g = graph_initialize(1.0, size);
+        //printf(" to %ld\n", to);
+        //printf("To %s\n", tok);
 
-    rewind(stream);
-
-    while ((fgets(line, LINE_SIZE, stream)) != NULL)
-    {
-        if (line[0] == '#')
-        {
-            continue;
-        }
-
-        tok = strtok(line, "\r \t\v");
-        from = atol(tok);
-        //printf("From %ld to ", from);
-
-        tok = strtok(NULL, "\r \t\v");
-        to = atol(tok);
-        //printf("%ld\n", to);
         graph_add_link(g, from, to);
+        counter++;
     }
 
     fclose(stream);
-    return g;
 }
 
-void *pagerank_calculate(void *arg)
+void write_csv(char *filename, graph_t *g)
 {
-    thread_data *data;
-    node_t *nodes;
-    link_t *link;
-    long i, j;
-    double score;
+    FILE *stream;
 
-    data = (thread_data *)arg;
-    //printf("Managing from: %ld to %ld.\n", data->from, data->to);
-
-    assert(g && g->nodes);
-
-    nodes = g->nodes;
-
-    for (int j = 0; j < ITERATIONS; j++)
+    if (!(stream = fopen(filename, "w+")))
     {
-
-        pthread_barrier_wait(&bar);
-
-        for (i = data->from; i < data->to; i++)
-        {
-            if (nodes[i].outlinks_num == 0)
-            {
-                continue;
-            }
-
-            //calc for nodes[i].
-            score = (nodes[i].score * D_FACTOR) / nodes[i].outlinks_num; //need sync
-            //printf("Node %ld will give %f score to:\n", i, score);
-
-            //assign
-            link = nodes[i].outlinks_head;
-            while (link != NULL)
-            {
-                //printf("    Link %ld before: %f\n", link->to_id, nodes[link->to_id].score_next);
-                nodes[link->to_id].score_next += score; //need sync
-                //printf("    Link %ld after (next): %f\n", link->to_id, nodes[link->to_id].score_next);
-
-                link = link->next;
-            }
-
-            nodes[i].score_next -= nodes[i].score * D_FACTOR;
-            //printf("    Node after update: %f\n", nodes[i].score_next);
-        }
-
-        pthread_barrier_wait(&bar);
-
-        for (i = data->from; i < data->to; i++)
-        {
-            nodes[i].score = nodes[i].score_next;
-            //nodes[i].score_next = 0;
-        }
+        perror("Could not open the file");
+        exit(EXIT_FAILURE);
     }
-    return NULL;
+
+    graph_output(g, stream);
+
+    fclose(stream);
 }
